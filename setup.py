1b621d205bb75f82b2b7e04356723b00bf15c72b
Riccardo Finotello <riccardo.finotello@gmail.com>
2020-12-21 19:28:45 +0100

    Update setup file
    Signed-off-by: Riccardo Finotello <riccardo.finotello@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(name='nntools',
-                 version='0.1.0',
+                 version='0.1.1',
                  author='Riccardo Finotello',
                  author_email='riccardo.finotello@gmail.com',
                  description='Tools for deep learning and data analysis',
